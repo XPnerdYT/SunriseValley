5ab3da91270c8823096a41ac89405605b9ddfdc7
reCODEn <reagan.noonan@student.tdsb.on.ca>
2025-05-19 15:31:11 -0400

    Update GridMovement.py

--- GridMovement.py
+++ GridMovement.py
@@ -9,6 +9,8 @@ screen = pygame.display.set_mode(size)
 
 #colours
 RED = (255,0,0)
+GRASS1 = (51, 138, 55)
+GRASS2 = (29, 102, 32)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
 BLACK = (0,0,0)
@@ -16,41 +18,46 @@ WHITE = (255,255,255)
 
 #setting grid
 def grid_draw(pX,pY):
-    grid = [[0,1,0,1,0,1,0,1],
-            [1,0,1,0,1,0,1,0],
-            [0,1,0,1,0,1,0,1],
-            [1,0,1,0,1,0,1,0],
-            [0,1,0,1,0,1,0,1],
-            [1,0,1,0,1,0,1,0],
-            [0,1,0,1,0,1,0,1],
-            [1,0,1,0,1,0,1,0]]            
+    row = []
+    for i in range(16):
+        if i % 2 == 0:
+            row.append(0)
+        elif i % 2 == 1:
+            row.append(1)
+    rowR = list(reversed(row))
+    grid = []
+    for i in range(16):
+        if i%2 == 0:
+            grid.append(row[:])
+        elif i%2 == 1:
+            grid.append(rowR[:])            
     grid[pY][pX] = 2
     for y in range(len(grid)):
         for x in range(len(grid[y])):
             if grid[y][x] == 0:
-                square = BLACK
+                square = GRASS1
             elif grid[y][x] == 1:
-                square = WHITE
+                square = GRASS2
             elif grid[y][x] == 2:
                 square = RED
-            pygame.draw.rect(screen,square,[x*100,y*100,100,100])
+            pygame.draw.rect(screen,square,[x*50,y*50,50,50])
 
 #User input for movement            
 def player_movement(pX, pY):
     keys = pygame.key.get_pressed()
-    pygame.time.delay(80)
+    pygame.time.delay(50)
     
     if keys[pygame.K_a]: 
-        if 0 < pX <= 7:
+        if 0 < pX <= 15:
             pX -= 1
     elif keys[pygame.K_d]: 
-        if 0 <= pX < 7:
+        if 0 <= pX < 15:
             pX += 1
     elif keys[pygame.K_w]: 
-        if 0 < pY <= 7:
+        if 0 < pY <= 15:
             pY -= 1
     elif keys[pygame.K_s]: 
-        if 0 <= pY < 7:
+        if 0 <= pY < 15:
             pY += 1
             
     return pX, pY
@@ -60,6 +67,7 @@ pY = 0
 clock = pygame.time.Clock()  
 running = True
 while running:
+    screen.fill(BLACK)
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
