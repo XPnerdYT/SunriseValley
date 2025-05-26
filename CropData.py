6dc99d7e0836bf5811f140cb447d3f7d4f2abbfd
XPnerd <87585714+XPnerdYT@users.noreply.github.com>
2025-05-26 09:21:53 -0400

    Update CropData.py

--- CropData.py
+++ CropData.py
@@ -1,25 +1,31 @@
 CROP_DATA = {
     'carrot': {
         'growth_stages': [200,400,600],
-        'sell_price': 5,
+        'sell_price': 3,
         'seed_cost': 2,
         'display_name': 'carrot',
         'max_stage': 3,
         'renewable': False,
         },
     'potato': {
-        'gtowth_stages': [400,800],
-        'sell_price' : 8,
+        'gtowth_stages': [500,1000],
+        'sell_price' : 6,
         'seed_cost': 3,
         'display_name': 'potato',
         'max_stage': 2,
         'renewable': False,
         },
     'tomato': {
-        'growth_stages': [100,300,500,1000]
+        'growth_stages': [100,300,500,1500],
         'sell_price': 4,
-        'seed_cost': 5,
+        'seed_cost': 6,
         'display_name': 'tomato',
         'max_stage': 4,
-        'renewable': True
+        'renewable': True,
+        },
+    'blueberry': {
+        'growth_stages': [600,1200,1800,1920],
+        'sell_price': 2，
+        'seed_cost‘：10，
+        
     }
