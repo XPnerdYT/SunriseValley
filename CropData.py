b1df21b479420e0c4eeb4e10d542ca09b7677197
XPnerd <87585714+XPnerdYT@users.noreply.github.com>
2025-05-27 09:53:04 -0400

    Update CropData.py

--- CropData.py
+++ CropData.py
@@ -7,6 +7,7 @@ CROP_DATA = {
         'max_stage': 3,
         'renewable': False,
         'image_path': 'crop.png',
+        'stage_images' : ['carrot_stage0.png','carrot_stage1.png','carrot_stage2.png','carrot_stage3.png']
         },
     'potato': {
         'gtowth_stages': [500,1000],
@@ -16,6 +17,7 @@ CROP_DATA = {
         'max_stage': 2,
         'renewable': False,
         'image_path': 'crop.png',
+        'stage_images' : ['potato_stage0.png','potato_stage1.png','potato_stage2.png''
         },
     'tomato': {
         'growth_stages': [200,400,800,1400],
@@ -25,6 +27,7 @@ CROP_DATA = {
         'max_stage': 4,
         'renewable': True,
         'image_path': 'crop.png',
+        'stage_images' : ['tomato_stage0.png','tomato_stage1.png','tomato_stage2.png','tomato_stage3.png','tomato_stage4.png]
         },
     'blueberry': {
         'growth_stages': [600,1200,1800,1900],
@@ -34,4 +37,5 @@ CROP_DATA = {
         'max_stage': 4,
         'renewable': True,
         'image_path': 'crop.png',
+        'stage_images' : ['blueberry_stage0.png','blueberry_stage1.png','blueberry_stage2.png','blueberry_stage3.png','blueberry_stage4.png']
     }
