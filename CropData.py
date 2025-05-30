c3b7e46527953c4abb1ab3343a785a30cddb66bc
reCODEn <reagan.noonan@student.tdsb.on.ca>
2025-05-29 20:57:40 -0400

    Add files via upload

--- CropData.py
+++ CropData.py
@@ -7,7 +7,6 @@ CROP_DATA = {
         'max_stage': 3,
         'renewable': False,
         'image_path': 'crop.png',
-        'stage_images' : ['carrot_stage0.png','carrot_stage1.png','carrot_stage2.png','carrot_stage3.png']
         },
     'potato': {
         'gtowth_stages': [500,1000],
@@ -17,25 +16,20 @@ CROP_DATA = {
         'max_stage': 2,
         'renewable': False,
         'image_path': 'crop.png',
-        'stage_images' : ['potato_stage0.png','potato_stage1.png','potato_stage2.png''
         },
     'tomato': {
-        'growth_stages': [200,400,800,1400],
+        'growth_stages': [100,300,500,1500],
         'sell_price': 4,
         'seed_cost': 6,
         'display_name': 'tomato',
         'max_stage': 4,
         'renewable': True,
         'image_path': 'crop.png',
-        'stage_images' : ['tomato_stage0.png','tomato_stage1.png','tomato_stage2.png','tomato_stage3.png','tomato_stage4.png]
         },
     'blueberry': {
-        'growth_stages': [600,1200,1800,1900],
+        'growth_stages': [600,1200,1800,1920],
         'sell_price': 2,
         'seed_cost':10,
-        'display_name': 'blueberry',
-        'max_stage': 4,
-        'renewable': True,
         'image_path': 'crop.png',
-        'stage_images' : ['blueberry_stage0.png','blueberry_stage1.png','blueberry_stage2.png','blueberry_stage3.png','blueberry_stage4.png']
     }
+}
--- FarmingGame.py
+++ FarmingGame.py
@@ -2,43 +2,38 @@
 #2025-05-23
 
 import pygame
-
+from Variables import *
 pygame.init()
 size = (1200,800)
 screen = pygame.display.set_mode(size)
 
-##Colours
-BLACK = (0,0,0)
-RED = (255,0,0)
-
-##Images
-farmingBG = pygame.image.load('FarmingBackground.png')
-farmingBG = pygame.transform.scale(farmingBG,[1200,800])
-
-##Creating the farming grid
 farming_grid = []
-for y in range(10):
+for y in range(12):
     farming_row = []
-    for x in range(10):
-        farming_row.append('empty')
+    for x in range(12):
+        farming_row.append('EMPTY')
     farming_grid.append(farming_row)
+
 for item in farming_grid:
     print(item)
 
-clock = pygame.time.Clock()
+def farming_hitbox(mouseX,mouseY):
+    for y in range(len(farming_grid)):
+        for x in range(len(farming_grid[y])):
+            if x%2 == 0 and y%2 == 0:
+                pygame.draw.rect(screen,GREEN,[75*x,75*y,75,75])
+            elif x%2 == 1 and y%2 == 1:
+                pygame.draw.rect(screen,GREEN,[75*x,75*y,75,75])
+            else:
+                pygame.draw.rect(screen,BLUE,[75*x,75*y,75,75])
+
+farming_hitbox(1,2)
+
+pygame.display.flip()
+
 running = True
 while running:
-    screen.fill(BLACK)
-    screen.blit(farmingBG,[0,0])
-    
-    ##Hitboxes for planting crops in the farming grid
-    for y in range(len(farming_grid)):
-        for x in range(len(farming_grid)):
-            pygame.draw.rect(screen,RED,[116+75*x-5*y,341+y*40-5*x,72,37],1)
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    pygame.display.flip()
-    clock.tick(60)
 pygame.quit()
